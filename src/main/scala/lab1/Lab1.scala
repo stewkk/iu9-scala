7a8ec44446513c5e6d5a31079bec6a12bf1fbc0f
Alexandr Starovoytov <aistarovoytov@yandex.ru>
2025-03-03 16:33:27 +0300

    Add zero support

--- src/main/scala/lab1/Lab1.scala
+++ src/main/scala/lab1/Lab1.scala
@@ -6,9 +6,17 @@ object Lab1 {
   }
 
   def mul: (List[Int], List[Int]) => List[Int] = {
-    case (Nil, Nil) => Nil
-    case (l :: lhs, Nil) => l :: mul(lhs, Nil)
-    case (Nil, r :: rhs) => r :: mul(Nil, rhs)
-    case (l :: lhs, r :: rhs) => l+r :: mul(lhs, rhs)
+    def mulNonZero: (List[Int], List[Int]) => List[Int] = {
+      case (Nil, Nil) => Nil
+      case (l :: lhs, Nil) => l :: mulNonZero(lhs, Nil)
+      case (Nil, r :: rhs) => r :: mulNonZero(Nil, rhs)
+      case (l :: lhs, r :: rhs) => l+r :: mulNonZero(lhs, rhs)
+    }
+
+    {
+      case (Nil, _) => Nil
+      case (_, Nil) => Nil
+      case (lhs, rhs) => mulNonZero(lhs, rhs)
+    }
   }
 }
--- src/test/scala/lab1/MulSuite.scala
+++ src/test/scala/lab1/MulSuite.scala
@@ -3,6 +3,8 @@ import lab1.Lab1
 class MulSuite extends munit.FunSuite {
   test("If both of the arguments is Nil, mul should return Nil") {
     assert(Lab1.mul(Nil, Nil) == Nil)
+    assert(Lab1.mul(Nil, List(1, 1)) == Nil)
+    assert(Lab1.mul(List(1), Nil) == Nil)
   }
 
   test("mul of two numbers with same length gives sum of its positions") {
@@ -12,7 +14,5 @@ class MulSuite extends munit.FunSuite {
 
   test("mul of numbers with different length complements smaller one with zeroes") {
     assert(Lab1.mul(List(1, 2), List(1, 2, 3)) == List(2, 4, 3))
-    assert(Lab1.mul(Nil, List(1, 1)) == List(1, 1))
-    assert(Lab1.mul(List(1), Nil) == List(1))
   }
 }
