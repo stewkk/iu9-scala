e1b5b0e824e909be32b26269630fc0269c0242b0
Alexandr Starovoytov <aistarovoytov@yandex.ru>
2025-03-21 13:20:53 +0300

    Impl inverse

--- lab2/src/main/scala/lab2/FreeGroup.scala
+++ lab2/src/main/scala/lab2/FreeGroup.scala
@@ -3,12 +3,12 @@ package lab2
 class FreeGroup private {
   private var _value = ""
 
-  def value: String = _value
-
   def this(v: String) = {
     this()
     _value = normalize(v)
   }
+
+  def value = _value
 
   private def normalize(str: String): String = {
     val res = str
@@ -24,4 +24,24 @@ class FreeGroup private {
 
   override def toString: String =
     s"$value"
+
+  def +(other: FreeGroup) = new FreeGroup(value+other.value)
+
+  def inverse() : FreeGroup = {
+    def inverseRec(prefix: String, suffix: String): String = {
+      suffix.splitAt(2) match {
+        case ("", rest) => prefix
+        case ("a", rest) => inverseRec(prefix+"̃a", rest)
+        case ("b", rest) => inverseRec(prefix+"̃b", rest)
+        case ("ã", rest) => inverseRec(prefix+"a", rest)
+        case ("b̃", rest) => inverseRec(prefix+"b", rest)
+        case ("ab", rest) => inverseRec(prefix+"̃a", "b"+rest)
+        case ("aa", rest) => inverseRec(prefix+"̃a", "a"+rest)
+        case ("ba", rest) => inverseRec(prefix+"̃b", "a"+rest)
+        case ("bb", rest) => inverseRec(prefix+"̃b", "b"+rest)
+      }
+    }
+
+    new FreeGroup(inverseRec("", value).reverse)
+  }
 }
--- lab2/src/test/scala/lab2/FreeGroupSuite.scala
+++ lab2/src/test/scala/lab2/FreeGroupSuite.scala
@@ -12,4 +12,19 @@ class FreeGroupSuite extends munit.FunSuite {
     assert(new lab2.FreeGroup("̃ãa").value == "")
     assert(new lab2.FreeGroup("aaãã").value == "")
   }
+
+  test("Add concatenates and normalizes") {
+    val lhs = new lab2.FreeGroup("aba")
+    val rhs = new lab2.FreeGroup("ãb")
+
+    val got = lhs + rhs
+
+    assert(got.value == "abb")
+  }
+
+  test("Inverse inverses chars and reverses value") {
+    val got = new lab2.FreeGroup("abãb̃").inverse()
+
+    assert(got.value == new lab2.FreeGroup("bab̃ã").value)
+  }
 }
