66852ca6ebce66573924739fc4e9234ba9fa0d3e
Alexandr Starovoytov <aistarovoytov@yandex.ru>
2025-05-11 16:05:49 +0300

    Typo

--- lab3/src/test/scala/lab3/LinearInequalitySuite.scala
+++ lab3/src/test/scala/lab3/LinearInequalitySuite.scala
@@ -1,6 +1,6 @@
 import lab3.LinearInequality
 
-class FreeGroupSuite extends munit.FunSuite {
+class LinearInequalitySuite extends munit.FunSuite {
   test("Solve integers false") {
     val a = new lab3.LinearInequality(List(1, 1, 1), 5)
 
