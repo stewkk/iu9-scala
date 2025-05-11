5d0bb6657dfd2c9e0aa8f525501343ea5b0fd0d1
Alexandr Starovoytov <aistarovoytov@yandex.ru>
2025-05-11 19:21:47 +0300

    Impl simplify

--- lab4/src/main/scala/lab4/Lab4.scala
+++ lab4/src/main/scala/lab4/Lab4.scala
@@ -1,9 +1,11 @@
 package lab4
 
+import scala.math.pow
+
 abstract class Expr
 
 case class VarExpr(v: String) extends Expr
-case class NumExpr(v: Integer) extends Expr
+case class NumExpr(v: Int) extends Expr
 case class PlusExpr(lhs: Expr, rhs: Expr) extends Expr
 case class MinusExpr(lhs: Expr, rhs: Expr) extends Expr
 case class MulExpr(lhs: Expr, rhs: Expr) extends Expr
@@ -12,6 +14,41 @@ case class ExpExpr(lhs: Expr, rhs: Expr) extends Expr
 case class UnaryMinusExpr(expr: Expr) extends Expr
 
 object Main {
+  def simplify(expr: Expr): Expr = {
+    val simplified = expr match {
+      case e @ VarExpr(_) => e
+      case e @ NumExpr(_) => e
+      case e @ PlusExpr(lhs, rhs) => PlusExpr(simplify(lhs), simplify(rhs))
+      case e @ MinusExpr(lhs, rhs) => MinusExpr(simplify(lhs), simplify(rhs))
+      case e @ MulExpr(lhs, rhs) => MulExpr(simplify(lhs), simplify(rhs))
+      case e @ DivExpr(lhs, rhs) => DivExpr(simplify(lhs), simplify(rhs))
+      case e @ ExpExpr(lhs, rhs) => ExpExpr(simplify(lhs), simplify(rhs))
+      case e @ UnaryMinusExpr(expr) => UnaryMinusExpr(simplify(expr))
+    };
+    simplified match {
+      case PlusExpr(_ @ NumExpr(lhs), _ @ NumExpr(rhs)) => NumExpr(lhs+rhs)
+      case MinusExpr(_ @ NumExpr(lhs), _ @ NumExpr(rhs)) => NumExpr(lhs-rhs)
+      case MulExpr(_ @ NumExpr(lhs), _ @ NumExpr(rhs)) => NumExpr(lhs*rhs)
+      case DivExpr(_ @ NumExpr(lhs), _ @ NumExpr(rhs)) => NumExpr(lhs/rhs)
+      case ExpExpr(_ @ NumExpr(lhs), _ @ NumExpr(rhs)) => NumExpr(pow(lhs.toDouble, rhs.toDouble).toInt)
+      case UnaryMinusExpr(_ @ NumExpr(op)) => NumExpr(-op)
+      case PlusExpr(other, _ @ NumExpr(0)) => other
+      case PlusExpr(_ @ NumExpr(0), other) => other
+      case MinusExpr(other, _ @ NumExpr(0)) => other
+      case MinusExpr(_ @ NumExpr(0), _ @ UnaryMinusExpr(other)) => other
+      case MinusExpr(_ @ NumExpr(0), other) => simplify(UnaryMinusExpr(other))
+      case MulExpr(_ @ NumExpr(1), other) => other
+      case MulExpr(other, _ @ NumExpr(1)) => other
+      case MulExpr(_, zero @ NumExpr(0)) => zero
+      case MulExpr(zero @ NumExpr(0), _) => zero
+      case ExpExpr(_, _ @ NumExpr(0)) => NumExpr(1)
+      case ExpExpr(other, _ @ NumExpr(1)) => other
+      case ExpExpr(_ @ NumExpr(1), _) => NumExpr(1)
+
+      case other => other
+    }
+  }
+
   def main(args: Array[String]): Unit = {
     println("Hello")
   }
--- lab4/src/test/scala/lab4/SimplifySuite.scala
+++ lab4/src/test/scala/lab4/SimplifySuite.scala
@@ -12,4 +12,44 @@ class SimplifySuite extends munit.FunSuite {
   test("Create binary expr") {
     val a = new PlusExpr(new NumExpr(2), new NumExpr(2))
   }
+
+  test("Simplify addition to 0") {
+    val a = new PlusExpr(new VarExpr("x"), new NumExpr(0))
+
+    val got = Main.simplify(a)
+
+    assert(got == new VarExpr("x"))
+  }
+
+  test("Simplify substraction from 0") {
+    val a = new MinusExpr(new NumExpr(0), new VarExpr("x"))
+
+    val got = Main.simplify(a)
+
+    assert(got == new UnaryMinusExpr(new VarExpr("x")))
+  }
+
+  test("Simplify multiplication by 1") {
+    val a = new MulExpr(new NumExpr(1), new VarExpr("x"))
+
+    val got = Main.simplify(a)
+
+    assert(got == new VarExpr("x"))
+  }
+
+  test("Simplify multiplication by 0") {
+    val a = new MulExpr(new NumExpr(0), new VarExpr("x"))
+
+    val got = Main.simplify(a)
+
+    assert(got == new NumExpr(0))
+  }
+
+  test("Simplify expressions without variables") {
+    val a = MulExpr(new NumExpr(2), new NumExpr(2))
+
+    val got = Main.simplify(a)
+
+    assert(got == new NumExpr(4))
+  }
 }
