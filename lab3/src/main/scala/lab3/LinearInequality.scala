1101912a19c486c9b2c092217b0e56e94292a701
Alexandr Starovoytov <aistarovoytov@yandex.ru>
2025-05-11 15:49:44 +0300

    Impl LinearInequality for Boolean

--- lab3/src/main/scala/lab3/LinearInequality.scala
+++ lab3/src/main/scala/lab3/LinearInequality.scala
@@ -1,10 +1,35 @@
 package lab3
 
-class LinearInequality[T](val a: List[T], b: T) (implicit num: Numeric[T] = null) {
+class LinearInequality[T](val a: List[T], b: T) (implicit num: LinearInequalityOps[T] = null) {
   def solve(v: List[T]): Boolean = {
-    val lhs = v.zip(a).foldLeft(num.fromInt(0))((accum, elem) => (accum, elem) match {
+    val lhs = v.zip(a).foldLeft(num.zero())((accum, elem) => (accum, elem) match {
                                                 case (accum, (x, y)) => num.plus(accum, num.times(x, y))
                                               })
     num.lt(lhs, b)
   }
+}
+
+trait LinearInequalityOps[T] {
+  def plus(a: T, b: T): T
+  def times(a: T, b: T): T
+  def lt(a: T, b: T): Boolean
+  def zero(): T
+}
+
+object LinearInequalityOps {
+  implicit def numeric_ops[T](implicit num: Numeric[T]): LinearInequalityOps[T] =
+    new LinearInequalityOps[T] {
+      def plus(a: T, b: T): T = num.plus(a, b)
+      def times(a: T, b: T): T = num.times(a, b)
+      def lt(a: T, b: T): Boolean = num.lt(a, b)
+      def zero(): T = num.fromInt(0)
+    }
+
+  implicit def boolean_ops: LinearInequalityOps[Boolean] =
+    new LinearInequalityOps[Boolean] {
+      def plus(a: Boolean, b: Boolean): Boolean = a || b
+      def times(a: Boolean, b: Boolean): Boolean = a && b
+      def lt(a: Boolean, b: Boolean): Boolean = !a && b
+      def zero(): Boolean = false
+    }
 }
