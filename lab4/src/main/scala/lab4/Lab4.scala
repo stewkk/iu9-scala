70fab741e212b2b7fe3324512b54d20ed75bb99b
Alexandr Starovoytov <aistarovoytov@yandex.ru>
2025-05-11 16:26:34 +0300

    Declare case classes

--- lab4/src/main/scala/lab4/Lab4.scala
+++ lab4/src/main/scala/lab4/Lab4.scala
@@ -2,6 +2,15 @@ package lab4
 
 abstract class Expr
 
+case class VarExpr(v: String) extends Expr
+case class NumExpr(v: Integer) extends Expr
+case class PlusExpr(lhs: Expr, rhs: Expr) extends Expr
+case class MinusExpr(lhs: Expr, rhs: Expr) extends Expr
+case class MulExpr(lhs: Expr, rhs: Expr) extends Expr
+case class DivExpr(lhs: Expr, rhs: Expr) extends Expr
+case class ExpExpr(lhs: Expr, rhs: Expr) extends Expr
+case class UnaryMinusExpr(expr: Expr) extends Expr
+
 object Main {
   def main(args: Array[String]): Unit = {
     println("Hello")
--- lab4/src/test/scala/lab4/SimplifySuite.scala
+++ lab4/src/test/scala/lab4/SimplifySuite.scala
@@ -1,6 +1,15 @@
-import lab4.Expr
+import lab4._
 
 class SimplifySuite extends munit.FunSuite {
-  test("Example") {
+  test("Create variable expr") {
+    val a = new VarExpr("x");
+  }
+
+  test("Create number expr") {
+    val a = new NumExpr(5);
+  }
+
+  test("Create binary expr") {
+    val a = new PlusExpr(new NumExpr(2), new NumExpr(2))
   }
 }
